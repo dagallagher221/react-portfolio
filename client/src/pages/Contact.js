2ba6622233a2437e187cf80b253a092c7950ff3e
Dan Gallagher <dagallagher221@gmail.com>
2020-02-02 17:10:46 -0500

    fixed some css issues

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//import "./contact.css";
+
+
+function Contact() {
+    return (
+        <a href="/" >
+            <div class="page">
+            <div>
+               Contact page coming soon
+            </div>
+            
+
+            </div></a>
+    ) 
+}
+   
+export default Contact;
