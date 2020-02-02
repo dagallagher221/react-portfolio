ee410ead541a1874cf8565b5bb37aa37ae9af3c5
Dan Gallagher <dagallagher221@gmail.com>
2020-02-02 16:00:45 -0500

    added packages, dependencies, .evn config, server, changed client folder to appropriate naming convention

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import {BrowsderRouter as Router, Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import "./App.css";
+
+const App = () => {
+  return(
+    <div className = "App">
+      <Router>
+        <div className="App-Router">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact}/>
+            <Route path="/projects" component={Projects}/>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const routes = require("./routes");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Express Session
+app.use(session({
+  secret: "secret",
+  resave: true,
+  saveUninitialized: true,
+}));
+
+// Connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+})
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// If deployed, use the deployed database. Otherwise use the local mongoSoap Database
+const MONGODB_URI = process.env.MONGODB_URI
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }); 
+
+// Define API routes here
+app.use(routes);
+app.use("/users", require("./routes/api/users"));
+
+// Send every other request to the React app
+// Define any API routes before this runs
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
