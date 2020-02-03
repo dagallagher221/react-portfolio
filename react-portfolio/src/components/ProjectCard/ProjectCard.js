0e63a585543617589e3e5e9e1299e06b42285fca
Dan Gallagher <dagallagher221@gmail.com>
2020-02-03 00:35:22 -0500

    fixed error in project card generation

--- react-portfolio/src/components/ProjectCard/ProjectCard.js
+++ react-portfolio/src/components/ProjectCard/ProjectCard.js
@@ -9,12 +9,12 @@ function ProjectCard(props) {
     return (
       <div className="card">
         <div className="img-container">
-          <img alt={props.name} src={props.image} />
+          <img alt={props.title} src={props.image} />
         </div>
         <div className="content">
           <ul>
             <li>
-              <strong>Name:</strong> {props.name}
+              <strong>Name:</strong> {props.title}
             </li>
           </ul>
             <li>
